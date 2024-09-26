ae5b429f02f4af4a959e03ba9d871323b1e556b3
Shreyas <syasa3404@gmail.com>
2024-09-26 21:49:15 +0530

    updates

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,16 +3,25 @@ import ProjectBox from './ProjectBox';
 import FraudDetectionImage from '../images/FraudDetection.jpg';
 import HousePredictionImage from '../images/HousePrediction.jpg';
 import KidneyPredictionImage from '../images/KidneyPrediction.jpg';
-import './ProjectBox.css'; // Importing CSS
+import './ProjectBox'; // Import new CSS for heading
 
 const Projects = () => {
   return (
     <div>
-      <h1 className='projectHeading'>My <b>Projects</b></h1>
-      <div className='project'>
-        <ProjectBox projectPhoto={FraudDetectionImage} projectName="Fraud Detection" />
-        <ProjectBox projectPhoto={HousePredictionImage} projectName="House Prediction" />
-        <ProjectBox projectPhoto={KidneyPredictionImage} projectName="Kidney Prediction" />
+      <h1 className="projectHeading">My <b>Projects</b></h1>
+      <div className="project">        
+        <ProjectBox 
+          projectPhoto={FraudDetectionImage} 
+          projectName="Fraud Detection"
+        />
+        <ProjectBox 
+          projectPhoto={HousePredictionImage} 
+          projectName="House Prediction"
+        />
+        <ProjectBox 
+          projectPhoto={KidneyPredictionImage} 
+          projectName="Kidney Prediction"
+        />
       </div>
     </div>
   );
