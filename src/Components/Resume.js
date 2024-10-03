03d4246b9a3432f9cbe42d661e48c4b2d1492b45
Shreyas <syasa3404@gmail.com>
2024-10-03 16:52:59 +0530

    Resume updated

---
+++ src/Components/Resume.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import pdf from "../Shreyas CR_Resume sept 2024.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "150px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume">
+          {/* Displaying PDF using an iframe */}
+          <iframe
+            src={pdf}
+            width={width > 100 ? "60%" : "80%"}
+            height="400px"
+            title="Resume PDF"
+            style={{ border: "none" }}
+          ></iframe>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "150px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
