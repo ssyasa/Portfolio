7d3949e1523d3d020ac5d991ad94670262d450b3
Shreyas <syasa3404@gmail.com>
2024-10-10 23:07:44 +0530

    modified:   src/Components/Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,8 +24,6 @@ const Nav = () => {
         bar[0].classList.toggle("barOne");
         bar[1].classList.toggle("barTwo");
         bar[2].classList.toggle("barThree");
-        bar[2].classList.toggle("barFour");
-
         ham[0].classList.toggle("showNavbar");
     }
 
@@ -35,7 +33,7 @@ const Nav = () => {
         bar[0].classList.remove("barOne");
         bar[1].classList.remove("barTwo");
         bar[2].classList.remove("barThree");
-        bar[2].classList.toggle("barFour");
+        
         ham[0].classList.remove("showNavbar");
     }
     
@@ -50,14 +48,13 @@ const Nav = () => {
             <span className='bar'></span>
             <span className='bar'></span>
             <span className='bar'></span>
-            <span className='bar'></span>
+            
         </div>
 
         <ul className='NavbarLinks'>
             <li onClick={hideMenu}><Link to="/"><AiOutlineHome/> Home</Link></li>
             <li onClick={hideMenu}><Link to="/About"><BsPerson/> About</Link></li>
             <li onClick={hideMenu}><Link to="/Project"><BsCodeSlash/> Project</Link></li>
-            <li onClick={hideMenu}><Link to="/Resume"><CgFileDocument/>CV</Link></li>
             <li onClick={hideMenu}><Link to="/Services"><CgFileDocument/>Services</Link></li>
         </ul>
         
